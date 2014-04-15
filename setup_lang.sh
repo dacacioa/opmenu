b0a051f5f9c504f109c54dbb3df6abd346a3899a
root <root@narebril.servehttp.com>
2014-04-15 15:59:15 +0200

    Adding a new directory

---
+++ setup_lang.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+msgfmt po/es_ES.po --output-file /usr/share/locale/es/LC_MESSAGES/opmenu.mo
+msgfmt po/gl_ES.po --output-file /usr/share/locale/gl/LC_MESSAGES/opmenu.mo
+msgfmt po/ca_ES.po --output-file /usr/share/locale/ca/LC_MESSAGES/opmenu.mo
+
+exit
